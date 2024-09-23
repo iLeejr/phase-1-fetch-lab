7d971737e55af2503b533aa858e2a4afdc5dfa29
Kaleb Malone <kalebmalone7@gmail.com>
2024-09-23 09:38:03 -0700

    completed lab

--- index.js
+++ index.js
@@ -1,7 +1,25 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+  return fetch("https://anapioficeandfire.com/api/books")
+    .then(response => response.json())
+    .then(books => {
+      // for (const dataObj of books) {
+      //   const li = document.createElement("li")
+      //   const h3 = document.createElement("h3")
+      //   h3.textContent = dataObj.name
+      //   li.append(h3)
+      //   document.querySelector("main").append(li)
+      // }
+
+
+renderBooks(books)
+    })
+    .catch(error => alert(error))
 }
+
+
+
+
 
 function renderBooks(books) {
   const main = document.querySelector('main');
@@ -12,6 +30,6 @@ function renderBooks(books) {
   });
 }
 
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
   fetchBooks();
 });
